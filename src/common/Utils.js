c557409ce3f68bc9e25a4a95dd3c2a2ecc9b5556
gabbarr1999 <80463325+gabbarr1999@users.noreply.github.com>
2024-12-19 12:31:11 +0530

    Update Utils.js

--- src/common/Utils.js
+++ src/common/Utils.js
@@ -1,3 +1,16 @@
+import {Dimensions} from 'react-native';
+const {width, height} = Dimensions.get('screen');
+
 export const getLog = (title, value) => {
     console.log(`🚀 ~ ${title}`, value)
 }
+
+const guidelineBaseWidth = 375;
+const guidelineBaseHeight = 812;
+
+const horizontalScale = size => (width / guidelineBaseWidth) * size;
+const verticalScale = size => (height / guidelineBaseHeight) * size;
+const moderateScale = (size, factor = 0.5) =>
+  size + (horizontalScale(size) - size) * factor;
+
+export {horizontalScale, verticalScale, moderateScale};
